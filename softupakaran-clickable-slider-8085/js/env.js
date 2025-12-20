57413baab94a8e9a84eef46e003c354e295c418f
Subash107 <lamasubash107@gmail.com>
2025-12-21 02:12:09 +0545

    Fix Google client ID override on production

--- softupakaran-clickable-slider-8085/js/env.js
+++ softupakaran-clickable-slider-8085/js/env.js
@@ -1,10 +1,15 @@
 // runtime env (override in production by editing this file)
 (function(){
+  var host = window.location && window.location.hostname ? window.location.hostname : "";
+  var isLocal = host === "localhost" || host === "127.0.0.1";
+
   // Allow overriding API_BASE and GOOGLE_CLIENT_ID via localStorage during dev
-  try {
-    var lb = localStorage.getItem("SPK_API_BASE");
-    if (lb) window.API_BASE = lb;
-  } catch(_) {}
+  if (isLocal) {
+    try {
+      var lb = localStorage.getItem("SPK_API_BASE");
+      if (lb) window.API_BASE = lb;
+    } catch(_) {}
+  }
   // default API base
   window.API_BASE = window.API_BASE || "http://localhost:4000";
 
@@ -12,6 +17,9 @@
   var meta = document.querySelector('meta[name="google-client-id"]');
   var metaId = meta ? meta.content : null;
   // Or from localStorage
-  var lsId = null; try { lsId = localStorage.getItem("SPK_GOOGLE_CLIENT_ID"); } catch(_) {}
+  var lsId = null;
+  if (isLocal) {
+    try { lsId = localStorage.getItem("SPK_GOOGLE_CLIENT_ID"); } catch(_) {}
+  }
   window.GOOGLE_CLIENT_ID = window.GOOGLE_CLIENT_ID || metaId || lsId || "161160759250-tvcu3a09i8gtdeaia6gpk04jo343oiet.apps.googleusercontent.com";
 })();
