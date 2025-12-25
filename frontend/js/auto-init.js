ebff7fe138dbd84d17006c4961d5f8a66b687709
Subash107 <lamasubash107@gmail.com>
2025-12-26 00:16:03 +0545

    chore: reorganize repo layout

--- frontend/js/auto-init.js
+++
@@ -1,7 +0,0 @@
-
-(function(){
-  const s = document.createElement("script");
-  s.src = "js/scroll-animate.js";
-  s.defer = true;
-  document.body.appendChild(s);
-})();
--- frontend/js/main.js
+++
@@ -1,6 +0,0 @@
-// Placeholder entry point for home page behavior.
-(function () {
-  if (document && document.documentElement) {
-    document.documentElement.classList.add("js-ready");
-  }
-})();
